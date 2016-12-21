44fc9e34b2f3ae93b8678803217401a0ae0c37bd
yywang1 <yywang1@i9i8.com>
2016-12-21 18:27:04 +0800

    进行中

--- res/tool/pluginshot/4/recite.php
+++ res/tool/pluginshot/4/recite.php
@@ -1,8 +1,10 @@
 <?
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/includes/global.init.php';
+include_once './func.php';
 
 $act = isset($_REQUEST['act']) && $_REQUEST['act'] ? $_REQUEST['act'] : '';
 $id = isset($_REQUEST['id']) && $_REQUEST['id'] ? $_REQUEST['id'] : '';
+$status = isset($_REQUEST['status']) && $_REQUEST['status'] ? $_REQUEST['status'] : 0;
 
 $dirPath = str_replace('\\', '/', __DIR__) . '/'; //当前文件所在目录
 $dicPath = $dirPath . 'list/' . $id . '.json';
@@ -10,18 +12,17 @@ $dicPath = $dirPath . 'list/' . $id . '.json';
 $dictionaries = file_get_contents($dirPath . 'dictionaries.json');
 $dictionaries = ($dictionaries != '') ? json_decode($dictionaries, true) : array();//词典列表数组
 
-switch ($act) {
-    case 'saveDic':
-        $r = array('code' => 0, 'msg' => '成功');
-        echo json_encode($r);
-        die;
+$list = file_get_contents($dirPath . 'list/' . $id . '.json');
+$list = ($list != '') ? json_decode($list, true) : array();//词典列表数组
 
+switch ($act) {
     default:
         //初始打开页面
         include_once $_SERVER['DOCUMENT_ROOT'] . '/php/webapp/snippets/pageInfo.php';
         $tplArray['sub_twig'] = "{$cat_name}/pluginshot/{$page_id}/";
         $tplArray['res_path'] = $container['WEB_ROOT'] . "res/{$cat_name}/pluginshot/{$page_id}/";
 
+        //词典信息
         $dic = array();
         foreach ($dictionaries as $dictionary) {
             if ($dictionary['id'] == $id) {
@@ -29,15 +30,19 @@ switch ($act) {
                 break;
             }
         }
-        //没有获取到相应词典数据时
         if(empty($dic)) {
-            echo $container['twig']->render("{$cat_name}/pluginshot/{$page_id}/dictation-error.html", $tplArray);
+            echo $container['twig']->render("{$cat_name}/pluginshot/{$page_id}/inexistent.html", $tplArray);
             die;
         }
-
         $tplArray['dic'] = $dic;
 
-        echo $container['twig']->render("{$cat_name}/pluginshot/{$page_id}/dictation.html", $tplArray);
+        //100个未默写单词数组
+        $tplArray['words'] = getWords($list, $status, 100);
+
+        //其它变量
+        $tplArray['status'] = $status;
+
+        echo $container['twig']->render("{$cat_name}/pluginshot/{$page_id}/recite.html", $tplArray);
         break;
 }
 
